7f2e0d7d58f8c8807fb0129eeb2a2e95ce1a9cba
Felix Dahlke <felix.dahlke@viaboxx.de>
2011-12-22 11:00:10 +0100

    Improve spec test failure message

--- src/test/clojure/clostache/test_specs.clj
+++ src/test/clojure/clostache/test_specs.clj
@@ -3,6 +3,8 @@
         clostache.parser)
   (:require [clj-yaml.core :as yaml]))
 
+;; TODO: Print a warning instead of an exception when the specs are missing
+
 (defn- load-spec-tests [spec]
   (let [path (-> (Thread/currentThread)
                  (.getContextClassLoader)
@@ -13,7 +15,8 @@
 (defn run-spec-test [spec-test]
   (is (= (:expected spec-test)
          (render (:template spec-test) (:data spec-test)))
-      (str (:name spec-test) ": " (:desc spec-test))))
+      (str (:name spec-test) ": " (:desc spec-test) "\nTemplate:\n---\n"
+           (:template spec-test) "\n---")))
 
 (defn run-spec-tests [spec]
   (doseq [spec-test (load-spec-tests spec)]
