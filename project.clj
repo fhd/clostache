9db1b2d240312a2e26ac7af2dc7d5c68549f5434
Michael S. Klishin <michael@novemberain.com>
2012-04-03 19:16:37 +0400

    Migrate to Leiningen 2
     * I went straight to Leiningen 2 because 1.7.1 may very well be the last 1.x release
    
     * Maven's pom.xml is still around but is added to .gitignore
    
     * I am not sure how to migrate <developers> and <scm> information to Leiningen (it should support it
       but I could not find any examples). If it matters then I will have to look into it further.
    
     * Because Maven stuff is not removed (at least not yet), I did not touch development-related section
       in the README

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject de.ubercode.clostache/clostache "1.3.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
+  :description "{{ mustache }} for Clojure"
+  :url "http://github.com/fhd/clostache"
+  :license {:name "GNU Lesser General Public License 2.1"
+            :url "http://www.gnu.org/licenses/lgpl-2.1.txt"
+            :distribution :repo}
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [jline/jline "0.9.94"]
+                 [org.clojure/data.json "0.1.2"]]
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  :resource-paths ["src/test/resources"]
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta6"]]}}
+  :aliases { "all" ["with-profile" "dev:dev,1.4"] }
+  :repositories {"clojure-releases" "http://build.clojure.org/releases",
+                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases",
+                             :snapshots false,
+                             :releases {:checksum :fail, :update :always}}})
