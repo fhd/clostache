e5389e0d8b43aee778d6540e955184b7b92a1998
Felix H. Dahlke <fhd@ubercode.de>
2016-09-28 14:32:04 +0200

    Specify warn-on-reflection via global-vars

--- project.clj
+++ project.clj
@@ -13,4 +13,4 @@
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}}
   :repositories {"clojure-releases" "http://build.clojure.org/releases"}
   :aliases {"all" ["with-profile" "dev:dev,1.5"]}
-  :warn-on-reflection true)
+  :global-vars {*warn-on-reflection* true})
